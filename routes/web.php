9ccbee4fa9ce94227efc11cde2183d29ec809d29
Vladislava <vladi.popova2004@yandex.ru>
2024-05-20 20:58:49 +0500

    .

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/info') -> group (function(){
+    Route::get('/server', [InfoController::class, 'serverInfo']);
+
+    Route::get('/client', [InfoController::class, 'clientInfo']);
+
+    Route::get('/database', [InfoController::class, 'databaseInfo']);
+});
